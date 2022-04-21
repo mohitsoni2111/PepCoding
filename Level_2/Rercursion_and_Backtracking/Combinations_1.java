18965cd39c6ce56606d1783a5962720a93070e8f
Mohit Soni <mohitsoni2111@gmail.com>
2022-04-21 18:10:37 +0530

    Level 2 Progress

---
+++ Level_2/Rercursion_and_Backtracking/Combinations_1.java
@@ -0,0 +1,19 @@
+package Level_2.Rercursion_and_Backtracking;
+import java.io.*;
+import java.util.*;
+
+public class Combinations_1 {
+    public static void combinations(int cb, int tb, int ssf, int ts, String asf){
+
+
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int nboxes = Integer.parseInt(br.readLine());
+        int ritems = Integer.parseInt(br.readLine());
+        combinations(1, nboxes, 0, ritems, "");
+    }
+}
--- Level_2/Rercursion_and_Backtracking/Tug_Of_War.java
+++ Level_2/Rercursion_and_Backtracking/Tug_Of_War.java
@@ -5,9 +5,27 @@ public class Tug_Of_War {
     static int mindiff = Integer.MAX_VALUE;
     static String ans = "";
 
-    public static void solve(int[] arr, int vidx, ArrayList<Integer> set1, ArrayList<Integer> set2, int soset1, int soset2) {
-        //write your code here
+    public static void solve(int[] arr, int vidx, ArrayList<Integer> set1,
+                             ArrayList<Integer> set2, int soset1, int soset2) {
+        if (vidx == arr.length){
+            int delta = Math.abs(soset1 - soset2);
+            if (delta < mindiff){
+                mindiff = delta;
+                ans = set1 + " " + set2;
+            }
+            return;
+        }
 
+        if (set1.size() < (arr.length + 1) / 2) {
+            set1.add(arr[vidx]);
+            solve(arr, vidx + 1, set1, set2, soset1 + arr[vidx], soset2);
+            set1.remove(set1.size() - 1);
+        }
+        if (set2.size() < (arr.length + 1) / 2) {
+            set2.add(arr[vidx]);
+            solve(arr, vidx + 1, set1, set2, soset1, soset2 + arr[vidx]);
+            set2.remove(set2.size() - 1);
+        }
     }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -18,5 +36,4 @@ public class Tug_Of_War {
         solve(arr, 0, new ArrayList<>(), new ArrayList<>(), 0, 0);
         System.out.println(ans);
     }
-
 }
